e82e19817ada88811a1c6a40085584506e9a4b68
xust <1214326397@qq.com>
2025-08-06 22:16:00 +0800

    build文件

--- compiler-rt/lib/fuzzer/build_optimization.sh
+++ compiler-rt/lib/fuzzer/build_optimization.sh
@@ -2,7 +2,7 @@
 LIBFUZZER_SRC_DIR=$(dirname $0)
 CXX="${CXX:-clang++}"
 for f in $LIBFUZZER_SRC_DIR/*.cpp; do
-  $CXX -stdlib=libc++ -fPIC -O2 -std=c++11 $f -c &
+  $CXX -stdlib=libc++ -fPIC -O2 -std=c++17 $f -c &
 done
 wait
 rm -f libFuzzer.a
